ffab878b6f22adf817a4804294bb971f3f05482e
Matheus Fracaroli <matheusfracaroli@mbp-de-matheus.senac.corp>
2022-04-19 22:11:19 -0300

    models

---
+++ Models/Ativo.class.php
@@ -0,0 +1,25 @@
+<?php 
+require __DIR__ . '/Model.class.php';
+
+abstract class Ativo extends Model {
+    public function __construct()
+    {
+        parent::__construct;
+    }
+
+    function inserir(array $dados):?int {
+        return null;
+    }
+
+    function atualizar(int $id, array $dados):bool {
+        return true;
+    }
+
+    function apagar(int $id):bool {
+        return true;
+    } 
+
+    function listar(int $id = null):?array {
+        return null;
+    }
+}
